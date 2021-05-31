f1306ac475359e9b10b4ab47c0ffe52af41b1a81
Matthias Roming <matze.ro99@gmail.com>
2021-05-31 17:16:34 +0200

    added class Trigger

---
+++ scripts/Trigger.js
@@ -0,0 +1,15 @@
+"use strict";
+var PuzzleGame;
+(function (PuzzleGame) {
+    class Trigger extends PuzzleGame.f.Node {
+        constructor(_pos, _size) {
+            super("Trigger");
+            this.addComponent(new PuzzleGame.f.ComponentTransform());
+            this.mtxLocal.translate(_pos);
+            this.mtxLocal.scale(_size);
+            this.addComponent(new PuzzleGame.f.ComponentRigidbody(0, PuzzleGame.f.PHYSICS_TYPE.STATIC, PuzzleGame.f.COLLIDER_TYPE.CUBE, PuzzleGame.f.PHYSICS_GROUP.TRIGGER));
+        }
+    }
+    PuzzleGame.Trigger = Trigger;
+})(PuzzleGame || (PuzzleGame = {}));
+//# sourceMappingURL=Trigger.js.map
---
+++ scripts/Trigger.ts
@@ -0,0 +1,14 @@
+namespace PuzzleGame {
+  export class Trigger extends f.Node {
+
+    constructor(_pos: f.Vector3, _size: f.Vector3) {
+      super("Trigger");
+
+      this.addComponent(new f.ComponentTransform());
+      this.mtxLocal.translate(_pos);
+      this.mtxLocal.scale(_size);
+      
+      this.addComponent(new f.ComponentRigidbody(0, f.PHYSICS_TYPE.STATIC, f.COLLIDER_TYPE.CUBE, f.PHYSICS_GROUP.TRIGGER));
+    }
+  }
+}
